e3dda4044c9d173b5ce580c12fe92d524f77d3f7
ray2 <49800069+takuyats@users.noreply.github.com>
2024-04-06 16:03:12 +0900

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+// ブログ一覧ページ（ホーム）
+import { getAllPosts } from "../lib/index";
+import { BlogList } from "@/components/BlogList/BlogList";
+import { NotionPost } from "@/types/NotionPost";
+
+export const revalidate = 60;
+
+export default async function Home() {
+  const posts: NotionPost[] = await getAllPosts();
+
+  return (
+    <div className="flex flex-col p-8 lg:w-4/6 mx-auto items-center">
+      <BlogList notionPosts={posts} />
+    </div>
+  );
+}
---
+++ src/lib/notion/notionClient.ts
@@ -0,0 +1,6 @@
+// notionにアクセスするためのclientを定義
+const { Client } = require("@notionhq/client");
+
+export const notion = new Client({
+  auth: process.env.NOTION_TOKEN,
+});
